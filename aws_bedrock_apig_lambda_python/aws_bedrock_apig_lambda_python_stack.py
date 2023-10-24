f6513f293e37f644b83649d95975c02108cc4772
Fred Liu <fred@liubojin.com>
2023-10-23 20:09:17 -0700

    initial implementation of wiring up claudev2 and bedrock.

--- aws_bedrock_apig_lambda_python/aws_bedrock_apig_lambda_python_stack.py
+++ aws_bedrock_apig_lambda_python/aws_bedrock_apig_lambda_python_stack.py
@@ -1,6 +1,8 @@
 from constructs import Construct
 from aws_cdk import (
+    Duration,
     App, Stack,
+    aws_iam as _iam,
     aws_lambda as _lambda,
     aws_apigateway as _apigw
 )
@@ -12,11 +14,13 @@ class AwsBedrockApigLambdaPythonStack(Stack):
         base_lambda = _lambda.Function(self, 'AwsBedrockApigLambda',
                                        handler='lambda-handler.handler',
                                        runtime=_lambda.Runtime.PYTHON_3_9,
+                                       timeout=Duration.seconds(300),
                                        code=_lambda.Code.from_asset('lambda'))
 
         base_api = _apigw.RestApi(self, 'BedrockApiGatewayWithCors',
                                   rest_api_name='BedrockApiGatewayWithCors')
 
+        # setting up example entity with plan and key
         example_entity = base_api.root.add_resource(
             'example',
             default_cors_preflight_options=_apigw.CorsOptions(
@@ -36,16 +40,9 @@ class AwsBedrockApigLambdaPythonStack(Stack):
         )
         example_entity_lambda_integration = _apigw.LambdaIntegration(
             base_lambda,
-            proxy=False,
-            integration_responses=[
-                _apigw.IntegrationResponse(
-                    status_code="200",
-                    response_parameters={
-                        'method.response.header.Access-Control-Allow-Origin': "'*'"
-                    }
-                )
-            ]
+            proxy=True
         )
+        # setting up GET method for example entity
         example_entity.add_method(
             'GET', example_entity_lambda_integration,
             method_responses=[
@@ -57,4 +54,25 @@ class AwsBedrockApigLambdaPythonStack(Stack):
                 )
             ],
             api_key_required=True
+        )
+        # setting up POST method for example entity
+        example_entity.add_method(
+            'POST', example_entity_lambda_integration,
+            method_responses=[
+                _apigw.MethodResponse(
+                    status_code="200",
+                    response_parameters={
+                        'method.response.header.Access-Control-Allow-Origin': True
+                    }
+                )
+            ],
+            api_key_required=True
+        )
+
+        # grant the lambda permission to invoke Bedrock AI models
+        base_lambda.role.add_to_principal_policy(
+            _iam.PolicyStatement(
+                actions=['bedrock:InvokeModel'],
+                resources=['arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2']
+            )
         )
--- lambda/lambda-handler.py
+++ lambda/lambda-handler.py
@@ -1,5 +1,40 @@
+import boto3
+import json
+bedrock = boto3.client(service_name='bedrock-runtime')
+
 def handler(event, context):
+    # if the method is GET, return a 200 OK with a message
+    if event['httpMethod'] == 'GET':
+        return {
+            'statusCode': 200,
+            'body': 'Lambda was invoked successfully through GET.'
+        }
+    if event['httpMethod'] == 'POST':
+        # if the method is POST, retrieve the body of the request
+        prompt = event['body']
+        body = json.dumps({
+            "prompt": f"\n\nHuman:{prompt}\n\nAssistant:",
+            "max_tokens_to_sample": 300,
+            "temperature": 0.1,
+            "top_p": 0.9,
+            "stop_sequences": [ "\\n\\nHuman:"]
+        })
+
+        modelId = 'anthropic.claude-v2'
+        accept = 'application/json'
+        contentType = 'application/json'
+
+        response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+
+        response_body = json.loads(response.get('body').read())
+        # text
+        # completion = response_body.get('completion')
+
+        return {
+            'statusCode': 200,
+            'body': f'{response_body}'
+        }
     return {
         'statusCode': 200,
-        'body': 'Lambda was invoked successfully.'
+        'body': f'Lambda was invoked successfully. {event=}, {context}'
     }
