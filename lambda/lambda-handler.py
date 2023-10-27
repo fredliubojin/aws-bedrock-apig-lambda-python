889282d9b7b4b13f1a49e3a2f5da585b203ed1e1
Fred Liu <fred@liubojin.com>
2023-10-26 22:34:34 -0700

    Refactor.

--- lambda/lambda-handler.py
+++ lambda/lambda-handler.py
@@ -1,61 +1,68 @@
 import boto3
 import json
 import logging
+
 bedrock = boto3.client(service_name='bedrock-runtime')
+modelId = 'anthropic.claude-v2'
+accept = 'application/json'
+contentType = 'application/json'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
-    # if the method is GET, return a 200 OK with a message
-    if event['httpMethod'] == 'GET':
-        return {
-            'statusCode': 200,
-            'body': 'Lambda was invoked successfully through GET.'
-        }
-    if event['httpMethod'] == 'POST':
-        # if the method is POST, retrieve the body of the request
-        body = event['body']
-        if not verify_json_fields(body,
-                                  ["prompt", "max_tokens_to_sample", "temperature"]):
-            # other potential fields "top_p", "top_k", "stop_sequences"
-            # log the error with body of the request
-            logging.error(f'Invalid JSON body: {json.loads(body)}')
-            # return a 400 Bad Request if the body does not contain the required fields
-            return {
-                'statusCode': 400,
-                'body': f'Invalid JSON body: {body=}'
-            }
-        # take out the "model", "stream"  field in the body if it exists, and convert it back to string
-        body_obj = json.loads(body)
-        if "model" in body_obj:
-            body_obj.pop("model")
-        stream_enabled = False
-        if "stream" in body_obj:
-            stream_enabled = body_obj["stream"]
-            body_obj.pop("stream")
-        body_obj['anthropic_version'] = 'bedrock-2023-05-31'
-        body = json.dumps(body_obj)
+    http_method = event.get('httpMethod')
 
-        modelId = 'anthropic.claude-v2'
-        accept = 'application/json'
-        contentType = 'application/json'
-        response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    if http_method == 'GET':
+        return _handle_get_request()
+    elif http_method == 'POST':
+        return _handle_post_request(event)
+    else:
+        return _handle_unsupported_http_method(http_method)
 
-        response_body = response.get('body').read().decode('utf-8')
-        if stream_enabled:
-            response_body = f'event: completion\ndata:{response_body}\n\n'
+def _handle_get_request():
+    return _create_response(200, 'Lambda was invoked successfully through GET.')
 
-        return {
-            'statusCode': 200,
-            'body': response_body
-        }
-    return {
-        'statusCode': 200,
-        'body': f'Lambda was invoked successfully. {event=}, {context}'
-    }
+def _handle_post_request(event):
+    body = event.get('body')
+    if not _is_valid_json_body(body, ["prompt", "max_tokens_to_sample", "temperature"]):
+        logger.error(f'Invalid JSON body: {json.loads(body)}')
+        return _create_response(400, f'Invalid JSON body: {body=}')
 
-def verify_json_fields(json_string, fields):
+    body_obj, stream_enabled = _process_body(body)
+    response = _invoke_model(body_obj)
+
+    if stream_enabled:
+        response = f'event: completion\ndata:{response}\n\n'
+
+    return _create_response(200, response)
+
+def _handle_unsupported_http_method(http_method):
+    logger.error(f'Unsupported HTTP method: {http_method}')
+    return _create_response(400, f'Unsupported HTTP method: {http_method}')
+
+def _is_valid_json_body(json_string, fields):
     try:
         data = json.loads(json_string)
     except json.JSONDecodeError:
         return False
 
     return all(field in data for field in fields)
+
+def _process_body(body):
+    body_obj = json.loads(body)
+    body_obj.pop("model", None)
+    stream_enabled = body_obj.pop("stream", False)
+    body_obj['anthropic_version'] = 'bedrock-2023-05-31'
+
+    return json.dumps(body_obj), stream_enabled
+
+def _invoke_model(body):
+    response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+    return response.get('body').read().decode('utf-8')
+
+def _create_response(status_code, body):
+    return {
+        'statusCode': status_code,
+        'body': body
+    }
